6c865ac790dafc309f15c129935ced624224858e
Jason Lee <jleeisme@gmail.com>
2017-04-13 11:43:43 -0700

    Added Kyle's face to the bouncing ball
    Added sunrays to his face also

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-// var f = document.getElementById('beachball7');
-// document.addEventListener('click', function(ev){
-//     f.style.transform = 'translateY('+(ev.clientY-25)+'px)';
-//     f.style.transform += 'translateX('+(ev.clientX-25)+'px)';
-// }, false);
-
 $(function() {
-  $('.kyle-popup').on('click', () => {
-    $('.popup').toggleClass('show-kyle');
+  $('.kyle-popup').on({'click': function() {
+    $('.popup').toggleClass('visible');
+    $('.beachball1').toggleClass('kyle-face');
+    
+    const imgSrc = ($('.beachball1').attr('src') === 'images/kyle.jpeg')
+      ? 'images/beachball.png'
+      : 'images/kyle.jpeg';
+    $('.beachball1').attr('src', imgSrc);
+  }
   });
 });
+// nope nope nope
+// $('.kyle-face').toggleClass('visible');
+// if (!$('.beachball1').hasClass('kyle-face')) {
+//   $('.kyle-face').addClass('show-kyle');
+
+//   $('.beachball1').addClass('kyle-face');
+// } 
+// else {
+//   $('.beachball1').removeClass('kyle-face');
+//   $('.kyle-face').removeClass('show-kyle');
+// }
+// $('.beachball1').toggleClass('kyle-face');
+// $('.kyle-face').toggleClass('show-kyle');
